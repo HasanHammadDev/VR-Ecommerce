65bbbc27cbac29e33e22b22c35f3263ebbc3973c
Hasan Hammad <hasanj2k@gmail.com>
2024-08-09 15:54:47 -0400

    Updated routes and wrapped the app in the auth provider context

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import Home from './components/Pages/Home';
 import Products from './components/Pages/Products';
 import { Routes, Route } from 'react-router-dom';
-import { CartProvider } from './components/context/context';
+import { CartProvider } from './components/context/CartContext';
 import Cart from './components/Pages/Cart';
 import ProductDetails from './components/Pages/ProductDetails';
+import Login from './components/Pages/Login';
 import Register from './components/Pages/Register';
+import { AuthProvider } from './components/context/AuthContext';
+import Profile from './components/Pages/Profile';
 
 const App: React.FC = () => {
   return (
-    <CartProvider>
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/products" element={ <Products /> } />
-        <Route path="/products/:id" element={ <ProductDetails /> } />
-        <Route path="/cart" element={ <Cart /> } />
-        <Route path="/register" element={ <Register /> } />
-      </Routes>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </CartProvider>
+    </AuthProvider>
+
   );
 };
 
