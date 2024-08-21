6925d26a54e0d68baa09ab0a91721fbce781ebdd
Hasan Hammad <hasanj2k@gmail.com>
2024-08-21 17:39:22 -0400

    Stored item count in local storage so it doesnt change back when page refreshes

--- src/components/context/CartContext.tsx
+++ src/components/context/CartContext.tsx
@@ -1,31 +1,33 @@
-import { createContext, useContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { ProviderProps, CartContextType } from '../../../types/types';
-
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
 export const useCart = () => {
-    const context = useContext(CartContext);
+    const context = React.useContext(CartContext);
     if (!context) {
         throw new Error('useCart must be used within a CartProvider');
     }
     return context;
 };
 
-
-
 export const CartProvider: React.FC<ProviderProps> = ({ children }) => {
-    const [itemCount, setItemCount] = useState<number>(0);
+    const [itemCount, setItemCount] = useState<number>(() => {
+        const storedItemCount = localStorage.getItem('itemCount');
+        return storedItemCount ? parseInt(storedItemCount, 10) : 0;
+    });
 
     const incrementItem = () => {
         setItemCount(prevCount => prevCount + 1);
     };
 
     const decrementItem = () => {
-        if (itemCount > 0) {
-            setItemCount(prevCount => prevCount - 1);
-        }
+        setItemCount(prevCount => Math.max(prevCount - 1, 0)); // Ensure itemCount does not go below 0
     };
+
+    useEffect(() => {
+        localStorage.setItem('itemCount', itemCount.toString());
+    }, [itemCount]);
 
     const contextValue: CartContextType = {
         itemCount,
